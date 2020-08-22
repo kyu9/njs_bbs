4f42a45e4cc680f97c9a64269daf6fb75220cca2
kyu9 <kyudo97@naver.com>
2020-08-22 20:58:28 +0900

    add s3

--- bbs/routes/write_receive_file.js
+++ bbs/routes/write_receive_file.js
@@ -1,7 +1,15 @@
 let express = require('express');
 let router = express.Router();
-let post = require('../function/post')
+let post = require('../function/post');
+const multerS3 = require('multer-s3');
 const multer = require('multer');
+const AWS = require('aws-sdk');
+AWS.config.update({
+    accessKeyId: 'AKIAZSCSHZ5FYML6ZV5M',
+    secretAccessKey: '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',
+    region: 'ap-northeast-2'
+})
+/*
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, '/Users/kyudoshim/mybbs/bbs/upload/')
@@ -11,6 +19,19 @@ var storage = multer.diskStorage({
     }
 })
 var upload = multer({storage: storage})
+
+ */
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'mybbs',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        acl: 'public-read',
+        key(req, file, cb){
+            cb(null, file.originalname)
+        }
+    })
+})
 
 router.post('/', upload.single('img'), function(req, res){
     post.wImg(req.body.title, req.body.content, req.body.uid, req.body.password, req.file.filename, function(result){res.json(result);})
