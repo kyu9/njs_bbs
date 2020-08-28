7e4902162d7f91d69ef3842a040f808a81399cf9
kyu9 <kyudo97@naver.com>
2020-08-28 17:36:20 +0900

    plus pagination

--- bbs/routes/posts.js
+++ bbs/routes/posts.js
@@ -1,11 +1,30 @@
-let express = require('express')
+let express = require('express');
 let models = require('../models');
+let fn = require('../function/paging');
 let router = express.Router()
 
 router.get('/',async(req,res, next)=>{
     try{
-        const posts = await models.post.findAll();
-        res.render('posts', {posts : posts, userid: req.session.user})
+        let pageNum = req.query.page;
+        console.log(pageNum);
+        let offset = 0;
+        console.log(pageNum);
+        if(pageNum>1){
+            offset = 7 * (pageNum - 1 );
+        }
+
+        await models.post.findAndCountAll({
+            offset: offset,
+            limit: 7
+        })
+            .then(result=>{
+                const posts = result.rows;
+                const count = result.count;
+                res.render('posts', {posts: posts, userid: req.session.user, pageNum: pageNum, count: count})
+            })
+
+        //const posts = await models.post.findAll();
+        //res.render('posts', {posts : posts, userid: req.session.user})
     }catch (e) {
         console.error(e);
         next(e);
